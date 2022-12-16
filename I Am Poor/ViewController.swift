f6bed2bde07a0c2fcb5cdf761bd9a662f4549731
tayjaf <tayyabx@gmail.com>
2022-12-15 19:56:23 -0500

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Tayyab Jafar on 2022-12-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
